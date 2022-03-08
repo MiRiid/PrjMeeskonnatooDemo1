4751a9bc48c795996f5b59597ec199c4354e9175
miriid <miriid@ICT-50120K>
2022-03-08 14:09:33 +0200

    guibui

--- PrjMeeskonnatooDemo1/TeisendajaAken.Designer.vb
+++ PrjMeeskonnatooDemo1/TeisendajaAken.Designer.vb
@@ -22,42 +22,73 @@ Partial Class TeisendajaAken
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblSisendYhik = New System.Windows.Forms.Label()
+        Me.lblVastusYhik = New System.Windows.Forms.Label()
+        Me.txtSisend = New System.Windows.Forms.TextBox()
+        Me.txtVastus = New System.Windows.Forms.TextBox()
+        Me.cmbTeisendus = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblSisendYhik
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(341, 132)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(44, 20)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Test!"
+        Me.lblSisendYhik.AutoSize = True
+        Me.lblSisendYhik.Location = New System.Drawing.Point(538, 102)
+        Me.lblSisendYhik.Name = "lblSisendYhik"
+        Me.lblSisendYhik.Size = New System.Drawing.Size(58, 20)
+        Me.lblSisendYhik.TabIndex = 0
+        Me.lblSisendYhik.Text = "Sisend"
         '
-        'Label2
+        'lblVastusYhik
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(341, 163)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(62, 20)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Test_2!"
+        Me.lblVastusYhik.AutoSize = True
+        Me.lblVastusYhik.Location = New System.Drawing.Point(538, 169)
+        Me.lblVastusYhik.Name = "lblVastusYhik"
+        Me.lblVastusYhik.Size = New System.Drawing.Size(59, 20)
+        Me.lblVastusYhik.TabIndex = 1
+        Me.lblVastusYhik.Text = "Vastus"
         '
-        'Form1
+        'txtSisend
+        '
+        Me.txtSisend.Location = New System.Drawing.Point(170, 102)
+        Me.txtSisend.Name = "txtSisend"
+        Me.txtSisend.Size = New System.Drawing.Size(100, 26)
+        Me.txtSisend.TabIndex = 2
+        '
+        'txtVastus
+        '
+        Me.txtVastus.Location = New System.Drawing.Point(170, 169)
+        Me.txtVastus.Name = "txtVastus"
+        Me.txtVastus.Size = New System.Drawing.Size(100, 26)
+        Me.txtVastus.TabIndex = 3
+        '
+        'cmbTeisendus
+        '
+        Me.cmbTeisendus.FormattingEnabled = True
+        Me.cmbTeisendus.Location = New System.Drawing.Point(170, 135)
+        Me.cmbTeisendus.Name = "cmbTeisendus"
+        Me.cmbTeisendus.Size = New System.Drawing.Size(121, 28)
+        Me.cmbTeisendus.TabIndex = 4
+        '
+        'TeisendajaAken
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Name = "Form1"
+        Me.Controls.Add(Me.cmbTeisendus)
+        Me.Controls.Add(Me.txtVastus)
+        Me.Controls.Add(Me.txtSisend)
+        Me.Controls.Add(Me.lblVastusYhik)
+        Me.Controls.Add(Me.lblSisendYhik)
+        Me.Name = "TeisendajaAken"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
+    Friend WithEvents lblSisendYhik As Label
+    Friend WithEvents lblVastusYhik As Label
+    Friend WithEvents txtSisend As TextBox
+    Friend WithEvents txtVastus As TextBox
+    Friend WithEvents cmbTeisendus As ComboBox
 End Class
