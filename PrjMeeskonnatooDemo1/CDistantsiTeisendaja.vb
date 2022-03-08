5fa1c58e23a8fcf3f05871c45558c599caba3d23
Parsifal22 <73550345+Parsifal22@users.noreply.github.com>
2022-03-08 14:25:17 +0200

    hhhh

---
+++ PrjMeeskonnatooDemo1/CDistantsiTeisendaja.vb
@@ -0,0 +1,11 @@
+﻿Public Class CDistantsiTeisendaja
+    Implements IDistantsiTeisendaja
+
+    Private Function KilometersToMiles(ByVal km As Double) As Double
+        Return (km * 1.6)
+    End Function
+    Private Function MilesToKilometers(ByVal mi As Double) As Double
+        Return (mi / 1.6)
+    End Function
+
+End Class
---
+++ PrjMeeskonnatooDemo1/IDistantsiTeisendaja.vb
@@ -0,0 +1,5 @@
+﻿Public Interface IDistantsiTeisendaja
+    Function KilometersToMiles(ByVal km As Double) As Double
+    Function MilesToKilometers(ByVal mi As Double) As Double
+
+End Interface
